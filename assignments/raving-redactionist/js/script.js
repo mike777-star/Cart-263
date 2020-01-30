2b426a4286215d486ff9775d1ce7af6ed7d7a979
Michael Sarlos <michaelsarlos@Michaels-MacBook-Pro.local>
2020-01-30 11:37:52 -0500

    A2: Step 8 Complete
    I also removed all of the previous comments.

--- assignments/raving-redactionist/js/script.js
+++ assignments/raving-redactionist/js/script.js
@@ -2,21 +2,14 @@
 
 /*****************
 
-Raving Redactionist
-Pippin Barr
-
-You are redacting a document, but it keeps becoming unredacted!
-Click the secret information to hide it, don't let all the
-secrets become revealed!
+Michael Sarlos (40018365)
+Assignment #2
 
 ******************/
 
-// The chance a span will be revealed per update
-const REVEAL_POSSIBILITY = 0.1;
-// How often to update the spans (potentially revealing them)
-const UPDATE_FREQUENCY = 500;
 
-// A place to store the jQuery selection of all spans
+const REVEAL_POSSIBILITY = 0.1;
+const UPDATE_FREQUENCY = 500;
 
 //Step 1: Adding my variables
 let secretsFound = 0;
@@ -28,14 +21,11 @@ let classifiedsFound;
 let $spans;
 let $secrets;
 
-// When the document is loaded we call the setup function
 $(document).ready(setup);
 
 // setup()
-//
-// Sets the click handler and starts the time loop
+
 function setup() {
-  // Save the selection of all spans (since we do stuff to them multiple times)
   $spans = $('span');
   //Step 2:
   //Selecting the secrets
@@ -52,37 +42,22 @@ function setup() {
   //Adding a mouseover event to all the secrets
   $secrets.on('mouseover', secretMouseover);
 
-  // Set a click handler on the spans (so we know when they're clicked)
   $spans.on('click', spanClicked);
-  // Set an interval of 500 milliseconds to update the state of the page
   setInterval(update, UPDATE_FREQUENCY);
-
 
 }
 
-// spanClicked()
-//
-// When a span is clicked we remove its revealed class and add the redacted class
-// thus blacking it out
 function spanClicked() {
   $(this).removeClass('revealed');
   $(this).addClass('redacted');
 }
 
-// update()
-//
-// Update is called every 500 milliseconds and it updates all the spans on the page
-// using jQuery's each() function which calls the specified function on _each_ of the
-// elements in the selection
+
 function update() {
   $spans.each(updateSpan);
 }
 
-// updateSpan()
-//
-// With random chance it unblanks the current span by removing the
-// redacted class and adding the revealed class. Because this function is called
-// by each(), "this" refers to the current element that each has selected.
+
 function updateSpan() {
   let r = Math.random();
   if (r < REVEAL_POSSIBILITY) {
@@ -94,6 +69,7 @@ function updateSpan() {
 //Step 3-4:
 //Creating my event handler function
 function secretMouseover(){
+  
 //Step 5:
 //Adding a found class to any secrets which are moused over
  $(this).addClass('found');
@@ -107,26 +83,11 @@ $(this).off('mouseover');
 //...variable changes
  secretsFound = secretsFound + 1;
 
+//Step 8:
+//Selecting the span which counts the secrets
+classifiedsFound = $('#classifiedsFound');
+//Using the text function I am setting the text to the value of the...
+//...secrets found
+classifiedsFound.text(secretsFound);
+
 }
-
-
-// A version using anonymous functions if you're interested:
-
-// $(document).ready(function () {
-//   $spans = $('span');
-//
-//   $spans.on('click',function () {
-//     $(this).removeClass('revealed');
-//     $(this).addClass('redacted');
-//   });
-//
-//   setInterval(function () {
-//     $spans.each(function () {
-//       let r = Math.random();
-//       if (r < REVEAL_POSSIBILITY) {
-//         $(this).removeClass('redacted');
-//         $(this).addClass('revealed');
-//       }
-//     });
-//   },UPDATE_FREQUENCY);
-// });
