fe55cfdc45efc7b34aa4b4d27e18d7fad6bee17a
Michael Sarlos <michaelsarlos@Michaels-MacBook-Pro.local>
2020-02-12 15:18:17 -0500

    P1: Commit 2
    I added my giveup button. I also added the jquery .on function so my start screen and buttons will trigger their repsective functions if clicked on. I also added my css classes so my rocket, buttons and background are currently positioned.

--- projects/project_1/js/script.js
+++ projects/project_1/js/script.js
@@ -3,16 +3,41 @@
 //My variables for my rockets and blast off button.
 let $rocket;
 let $rocketFire;
+//My blastoff button
 let $blastoff;
-
+//The giveup button
+let $giveup;
 
 
 
 $(document).ready(function() {
 
+//Using the jquery so the player can click on the start screen with...
+//...with the play function as the event handler.
+$("#start").on('click',play);
+//My initial play function to start the game on click
+function play(){
+//First step is removing the start id alongside it's start_screen image.
+  $("#start").remove();
+//Then the wrapper class(containing everything) is given the wrapper-play...
+//...class which adds the night_sky background
+  $(".wrapper").addClass("wrapper-play");
+  loadimage()
+//Declaring my button variables contain the proper id elements
+  $blastoff = $('#blastoff');
+  $giveup = $('#giveup');
 
+//Setting up my jquery .on functions so when clicked they trigger the proper...
+//...functions
+  $blastoff.on('click', takeoff);
+  $giveup.on('click', endgame);
+}
 
+//Creating my function which will end the game and show a message if the player..
+//...clicks give up.
+function endgame(){
 
+}
 
 });
 
