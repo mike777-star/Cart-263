cb17568671802c0a5a6460745c0f2254b05f98cf
Michael Sarlos <michaelsarlos@Michaels-MacBook-Pro.local>
2020-04-20 19:39:01 -0400

    Commit 2: Step 1-3
    I created my start screen and the events so when you click on it question 1 will appear. I set up question 1 so the form inputs will be sent to the answers array.

--- projects/project_3/js/script.js
+++ projects/project_3/js/script.js
@@ -6,7 +6,50 @@ $(document).ready(setup);
 
 function setup() {
 
-  $("#start").on('click', showInstructions);
+//Step 1: I am hiding all my questions so the start image can display first.
+  $("#robot").hide();
+  $("#questionOne").hide();
+  $("#questionTwo").hide();
+  $("#questionThree").hide();
+  $("#questionFour").hide();
+  $("#questionFive").hide
+
+};
+
+  function gotData(data){
+  let answers = [];
+  console.log(data);
+
+//Step 2: Here I am jquery functions so when the user click the start screen...
+//...it remove and it load the A.I. image alongside the first question.
+    $("#start").click(function(){
+    $("#robot").show();
+    $("#questionOne").show();
+    $("#start").hide();
+
+    });
+
+//Step 3: I am setting up my first question so the user can only input number...
+//...values into the form.
+    $("#submit-1").click(function(){
+  let inputValue  = $("#facebookFriend").val();
+  if (inputValue==="") {
+
+  }
+
+  else{
+//I am taking the answer and pushing it to my answers array
+    answers.push(inputValue);
+//I am changing the question value to the next one so the question inputs...
+//...don't get confused.
+    nextQuestion=2;
+//I am hiding the current question(1) and showing question two.
+    $("#questionOne").hide();
+    $("#questionTwo").show();
+
+  }
+
+});
 
 
   let options = {
@@ -22,4 +65,4 @@ function setup() {
     annyang.start();
     annyang.debug();
 
-  }
+  };
